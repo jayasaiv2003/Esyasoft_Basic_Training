1f28153ec1da3ce48cfc04463996ce569b759d3e
jayasaiv2003 <Veeramallu.rao@esyasoft.com>
2025-09-17 22:53:17 +0530

    banking app with oops

---
+++ .NET/OOPS_demo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Class_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Student first = new Student(1,"abcd",2,"12345678");
+            first.initialize();
+            first.showdisplay();
+        }
+    }
+}
+
+
---
+++ .NET/OOPS_demo/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_demo
+{
+    internal class Student
+    {
+        int id;
+        string name;
+        int standard;
+        string contactno;
+        public void initialize()
+        {
+            id=10;
+            name="abcd";
+
+        }
+        public void showdisplay()
+        {
+            Console.WriteLine($"student id is :{id}");
+            Console.WriteLine($"student name :{name}");
+            Console.WriteLine($"student standard :{standard}");
+            Console.WriteLine($"student contactno :{contactno}");
+
+        }
+        public void getmarks()
+        {
+
+        }
+        public Student(int id, string name, int standard, string contactno)
+        {
+            this.id=id;
+            this.name=name;
+            this.standard=standard;
+            this.contactno=contactno;
+        }
+        
+    }
+}

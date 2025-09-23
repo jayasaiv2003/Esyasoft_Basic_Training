7cb1d96af19128d2a055933d2e16ff1f98a4d899
jayasaiv2003 <Veeramallu.rao@esyasoft.com>
2025-09-23 09:47:15 +0530

    color_box_highlighter

---
+++ React/color_box_highlighter_ass_3/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ColorBoxHighlighter from './components/color_box_highlight';
+function App() {
+  return (
+    <div>
+      <ColorBoxHighlighter/>
+    </div>
+  );
+}
+
+export default App;
